195eb0e6242a5f5117bc32f5e1fdfc03b17cb347
Aaron <aaron@olkin.net>
2013-06-21 00:14:24 -0400

    Added database settings

--- socialvp/settings.py
+++ socialvp/settings.py
@@ -11,10 +11,10 @@ MANAGERS = ADMINS
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': '',                      # Or path to database file if using sqlite3.
-        'USER': '',                      # Not used with sqlite3.
-        'PASSWORD': '',                  # Not used with sqlite3.
+        'ENGINE': 'django.db.backends.mysql', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
+        'NAME': 'socialvp_django',                      # Or path to database file if using sqlite3.
+        'USER': 'socialvp_django',                      # Not used with sqlite3.
+        'PASSWORD': 'x7FZwTJURjJupJc4',                  # Not used with sqlite3. [GITWEB_OBFUSCATE]
         'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
         'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
     }
