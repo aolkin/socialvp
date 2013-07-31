beffafbd65089789f992f54ee9e3a92ad90ef5b4
Aaron <aaron@olkin.net>
2013-07-31 03:57:07 -0400

    Added apidocs folder to gh-pages branch

---
+++ apidocs/api.js
@@ -0,0 +1,18 @@
+YUI.add("yuidoc-meta", function(Y) {
+   Y.YUIDoc = { meta: {
+    "classes": [
+        "Plugin",
+        "svp"
+    ],
+    "modules": [
+        "SVP"
+    ],
+    "allModules": [
+        {
+            "displayName": "SVP",
+            "name": "SVP",
+            "description": "Social Video Player"
+        }
+    ]
+} };
+});
